fe505ff71d6bfd8226def02583da36c166af22ef
De La Josh <joshr1979@gmail.com>
2023-12-07 13:31:52 -0600

    intitial count button function +/- 1

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 
-let count = 5
+let count = 0
 
-countEl.textContent = count
+function counter() {
+    count += 1
+    countEl.textContent = count
+}
+
+function subtractor() {
+    count -= 1
+    countEl.textContent = count
+}
+
